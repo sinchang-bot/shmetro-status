00371f653e5ed0436902d2d21daad1911c3606d8
sinchang <sinchangwen@gmail.com>
2018-01-19 21:32:19 +0800

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# Shanghai Metro Status
+
+## 介绍
+
+每隔 2 分钟会去请求下上海地铁官方 API 去获取地铁运行情况，如果出现异常的话会推送消息到 Telegram Channel [@shmetro_status](https://t.me/shmetro_status)
+
+## 使用的服务
+
+- AWS CloudWatch
+- AWS Lambda
+- [apex](http://apex.run/)
---
+++ functions/sh/main.py
@@ -0,0 +1,26 @@
+import requests
+import os
+
+BOT_API_KEY = os.getenv('BOT_API_KEY')
+CHANNEL_NAME = os.getenv('CHANNEL_NAME')
+
+
+def fetch_status():
+  r = requests.post('http://service.shmetro.com/i/sm?method=doGetAllLineStatus')
+  return r.json()
+
+def send_message(text):
+  url = 'https://api.telegram.org/bot' + BOT_API_KEY +'/sendMessage?chat_id=@' + CHANNEL_NAME + '&text=' + text
+  requests.get(url)
+
+def handle(event, context):
+  try:
+    status = fetch_status()
+    for item in status:
+      if item['status'] != '0':
+        message = item['disLine'] + '号线运行出现异常，' + item['content']
+        send_message(message)
+    return
+  except Exception as e:
+    print(e)
+    return e
